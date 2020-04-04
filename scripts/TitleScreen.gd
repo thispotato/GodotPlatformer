29379019d25790321fdc9f5b6ba8b9c38798bdf4
small <maychild190@gmail.com>
2020-04-04 20:16:19 +0300

    Added StageOne. Connected TitleScreen to StageOne

--- scripts/TitleScreen.gd
+++ scripts/TitleScreen.gd
@@ -2,11 +2,22 @@ extends Node2D
 
 
 func _ready():
-	if $"Sprite/MarginContainer/HContainer/Play".grab_focus():
-		get_tree().quit()
+	#The game starts with focus on the play button
+	$Sprite/MarginContainer/HContainer/Play.grab_focus()
 	
 		
 	
-func _physics_process(delta):
+func _physics_process(_delta):
+	#on hover make the button clickable
 	if $"Sprite/MarginContainer/HContainer/Play".is_hovered():
-		get_tree().quit()
+		$Sprite/MarginContainer/HContainer/Play.grab_focus()
+	
+	if $"Sprite/MarginContainer/HContainer/Exit".is_hovered():
+		$Sprite/MarginContainer/HContainer/Exit.grab_focus()		
+		
+
+
+func _on_Play_pressed():
+	get_tree().change_scene("res://scenes/StageOne.tscn")
+func _on_Exit_pressed():
+	get_tree().quit()
