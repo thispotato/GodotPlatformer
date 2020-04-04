01e2e435eb10fdd479380e6eb033d37225a01471
small <maychild190@gmail.com>
2020-04-04 23:26:49 +0300

    Added Bullet scene + collision + visibility notifier

---
+++ scripts/Bullet.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+const SPEED = 150
+
+var velocity = Vector2()
+var direction = 1
+
+func set_bullet_direction(dir):
+	direction = dir
+	if dir == 1:
+		$Sprite.flip_h = false
+	else:
+		$Sprite.flip_h = true
+
+func _physics_process(delta):
+	velocity.x  = delta * SPEED * direction
+	translate(velocity)
+	
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	
+	queue_free()
